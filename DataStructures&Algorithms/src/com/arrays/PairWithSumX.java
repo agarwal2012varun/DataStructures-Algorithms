2056a075f2cc53967f10801d70fef1966d94a01a
Varun Agarwal <agarwalvarun2012@gmail.com>
2016-10-23 19:12:16 +0530

    Initial Checking of DS and Algos

---
+++ DataStructures&Algorithms/src/com/arrays/PairWithSumX.java
@@ -0,0 +1,12 @@
+package com.arrays;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+// http://www.geeksforgeeks.org/write-a-c-program-that-given-a-set-a-of-n-numbers-and-another-number-x-determines-whether-or-not-there-exist-two-elements-in-s-whose-sum-is-exactly-x/
+public class PairWithSumX {
+
+	public static void main(String[] args) {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+	}
+}
